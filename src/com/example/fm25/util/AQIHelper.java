5ea92bd36d4747a54a38c743f39f7a820049983e
aasdkl <hsuehchiahao@gmail.com>
2015-04-26 16:30:23 +0800

    新建基本字符串资源

---
+++ src/com/example/fm25/util/AQIHelper.java
@@ -0,0 +1,12 @@
+package com.example.fm25.util;
+
+public enum AQIHelper {
+	/*
+	 *     <string name="excellent">优</string>
+    <string name="good">良</string>
+    <string name="lightly_polluted">轻度污染</string>
+    <string name="moderately_polluted">中度污染</string>
+    <string name="heavily_polluted">重度污染</string>
+    <string name="severely_polluted">严重污染</string>
+	 */
+}
