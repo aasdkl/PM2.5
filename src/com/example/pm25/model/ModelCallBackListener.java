b724f79e19b6cd4a8318e9b168633ba69a217643
aasdkl <hsuehchiahao@gmail.com>
2015-04-30 14:01:24 +0800

    新增回调函数的注释

--- src/com/example/pm25/model/ModelCallBackListener.java
+++ src/com/example/pm25/model/ModelCallBackListener.java
@@ -2,10 +2,23 @@ package com.example.pm25.model;
 
 import java.util.List;
 
+/**
+ * 获取数据时所开线程的回调函数
+ * @author Administrator
+ * @param <T>
+ */
 public interface ModelCallBackListener<T> {
 
-	public void onFinish(final List<T> cities);
+	/**
+	 * 结束后返回值的处理
+	 * @param cities
+	 */
+	public void onFinish(final List<T> objs);
 
+	/**
+	 * 抛出的异常处理
+	 * @param e
+	 */
 	public void onError(final Exception e);
 
 }
