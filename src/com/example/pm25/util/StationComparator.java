930ad8b2a2ace833a887bbcbc4a53b9ce74eedb7
aasdkl <hsuehchiahao@gmail.com>
2015-04-30 13:56:16 +0800

    新增用于比较观测点的中文的StationCompator

---
+++ src/com/example/pm25/util/StationComparator.java
@@ -0,0 +1,23 @@
+package com.example.pm25.util;
+
+import java.text.Collator;
+import java.util.Comparator;
+
+import com.example.pm25.model.City;
+import com.example.pm25.model.Station;
+
+/**
+ * 对观测点的中文
+ * 直接对中文String排序: Comparator cmp =
+ * 	(RuleBasedCollator)java.text.Collator.getInstance(java.util.Locale.CHINA);
+ */
+public class StationComparator implements Comparator<Station> {
+
+	Collator cmp = Collator.getInstance(java.util.Locale.CHINA);
+
+	@Override
+	public int compare(Station lhs, Station rhs) {
+		return cmp.compare(lhs.getStationName(), rhs.getStationName()) ;
+	}
+
+}
