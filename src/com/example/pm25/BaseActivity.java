30c222ea7dd3045aa50bb9ea184fa753051aebf1
aasdkl <hsuehchiahao@gmail.com>
2015-05-04 23:43:54 +0800

    新增基Activity类，封装了 processDialog的操作

---
+++ src/com/example/pm25/BaseActivity.java
@@ -0,0 +1,36 @@
+package com.example.pm25;
+
+import com.example.pm25.util.MyLog;
+
+import android.app.Activity;
+import android.app.ProgressDialog;
+
+public class BaseActivity extends Activity {
+
+	public BaseActivity() {
+		MyLog.v("Which Activity is ?", getClass().getSimpleName());
+	}
+
+	private ProgressDialog progressDialog;
+	protected String dialogHint = "正在加载……";
+	
+	public void setDialogHint(String dialogHint) {
+		this.dialogHint = dialogHint;
+	}
+	
+	protected void showProgressDialog() {
+		if (progressDialog == null) {
+			progressDialog = new ProgressDialog(this);
+			progressDialog.setMessage(dialogHint);
+			progressDialog.setCanceledOnTouchOutside(false);
+		}
+		progressDialog.show();
+	}
+
+	protected void closeProgressDialog() {
+		if (progressDialog != null) {
+			progressDialog.dismiss();
+		}
+	}
+
+}
