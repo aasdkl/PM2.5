d0dae19cc7a5215f78a643e61a11009309705772
aasdkl <hsuehchiahao@gmail.com>
2015-05-08 11:38:59 +0800

    修改取消标题栏的方式，使用style替代Window.FEATURE_NO_TITLE

--- src/com/example/pm25/DetailActivity.java
+++ src/com/example/pm25/DetailActivity.java
@@ -71,7 +71,6 @@ public class DetailActivity extends BaseActivity {
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		requestWindowFeature(Window.FEATURE_NO_TITLE);
 		setContentView(R.layout.activity_detail);
 
 		Intent intent = getIntent();
--- src/com/example/pm25/MainActivity.java
+++ src/com/example/pm25/MainActivity.java
@@ -2,7 +2,6 @@ package com.example.pm25;
 
 import com.example.pm25.R;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.Window;
 
@@ -11,7 +10,6 @@ public class MainActivity extends BaseActivity {
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		requestWindowFeature(Window.FEATURE_NO_TITLE);
 		setContentView(R.layout.activity_main);
 	}
 
